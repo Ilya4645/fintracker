a5586486fe658600b1f1224b18ec2dd2fef5d961
Ilya4645 <irashidovchannel@gmail.com>
2025-11-25 15:34:35 +0300

    Modifec main.py, added commands.py

--- fintracker/main.py
+++ fintracker/main.py
@@ -1,1 +1,18 @@
-print('Hello, World!')
+import argparse
+import sys
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Финансовый трекер расходов')
+    parser.add_argument('name', help='Что вы хотите сделать? Введите цифру, соответствующую порядковому номеру команды: 1. Добавить запись')
+    parser.set_defaults(func=lambda args: print(f"Hello, {args.name}!"))
+
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(1)
+
+
+    args = parser.parse_args(args)
+    args.func(args)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
